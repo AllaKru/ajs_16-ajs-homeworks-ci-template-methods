19981e9224378dac06cb55723ed055dcbe56a72c
Alla Kruglova <renata-l@yandex.ru>
2021-07-06 19:28:49 +0300

    Исправила условия

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,17 +1,19 @@
 
 export default class Character {
   constructor(name, type) {
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    // this.attack = attack;
-    // this.defence = defence;
-
-    if (typeof this.name !== 'string' || this.name.length < 2 || this.name.length > 10 || (this.type !== 'Bowman'
-      && this.type !== 'Swordsman' && this.type !== 'Magician' && this.type !== 'Daemon' && this.type !== 'Undead'
-      && this.type !== 'Zombie')) {
-      throw new Error('Некорректные значения!');
+    if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
+      throw new Error('Некорректные значения! Name должно быть строкой, длиной более 2 и менее 10 символов');
+    } else if (type !== 'Bowman'
+      && type !== 'Swordsman' && type !== 'Magician' && type !== 'Daemon' && type !== 'Undead'
+      && type !== 'Zombie') {
+      throw new Error('Некорректные значения! Нет такого типа персонажа');
+    } else {
+      this.name = name;
+      this.type = type;
+      this.health = 100;
+      this.level = 1;
+      // this.attack = attack;
+      // this.defence = defence;
     }
   }
 }
